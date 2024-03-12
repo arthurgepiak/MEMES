4f5c532a10d98d63800725d5275ee7d5f8c47728
arthurgepiak <163136352+arthurgepiak@users.noreply.github.com>
2024-03-12 09:39:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Prick%C3%A4tarpucken.svg/800px-Prick%C3%A4tarpucken.svg.png"}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://cdn.jogos360.com.br/pa/cm/pac-man-d.jpg"}
+
+	
